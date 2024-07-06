cc42400c5c3346bc8a4711b69f583f96438dee15
hiillel <vicky.hillel26@gmail.com>
2024-07-06 14:54:22 +0200

    upd7

--- src/validation/contact.js
+++ src/validation/contact.js
@@ -23,5 +23,4 @@ export const contactUpdateValidationSchema = Joi.object({
   phoneNumber: Joi.string().min(3).max(20),
   email: Joi.string().email().min(3).max(20),
   isFavourite: Joi.boolean(),
-  contactType: Joi.string().valid('work', 'home', 'personal'),
 });
